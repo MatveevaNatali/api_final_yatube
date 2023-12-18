a9d14bd16126d690b3ebd0d665b1a3031ec06c35
MatveevaNatali <matveeva.skype@mail.ru>
2023-12-18 23:49:03 +0300

    Version2_18/12/2023

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,7 +7,7 @@ app_name = 'api'
 
 router_v1 = routers.DefaultRouter()
 router_v1.register(r'posts/(?P<post_id>[1-9]\d*)/comments', CommentViewSet,
-                basename='comments')
+                   basename='comments')
 router_v1.register(r'posts', PostViewSet)
 router_v1.register(r'groups', GroupViewSet)
 router_v1.register(r'follow', FollowViewSet, basename='follow')
